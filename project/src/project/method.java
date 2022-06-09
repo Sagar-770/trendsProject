884172d8dfc637cc8d32fe7921334517893443e1
Sagar <sagarvermaktl@gmail.com>
2022-06-08 18:27:01 -0700

    first commit

---
+++ project/src/project/method.java
@@ -0,0 +1,13 @@
+package project;
+
+public class method {
+	static void tellJoke() {
+		System.out.println("i invernt we word");
+	}
+
+	public static void main(String[] args) {
+		tellJoke();
+		
+	}
+
+}
